8289f7b2ab3f16e262d0af2167b920d88ab732fe
Karangwa Hirwa Julien <juliushirwa@gmail.com>
2020-01-03 18:00:55 +0200

    feat(verify): add verification link to verify email

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,5 +8,6 @@ router.post('/register', userMiddleware.register, userController.register);
 router.post('/role', userMiddleware.addRole, userController.addRole);
 router.get('/role', userController.getRole);
 router.post('/login', userController.login);
+router.post('/verification', userController.verification);
 
 export default router;
